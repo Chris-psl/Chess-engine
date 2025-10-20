ad6bdba077689e8c8091775f40ce7b976e576b67
Chris-psl <gregpsll12@gmail.com>
2025-10-20 11:28:53 +0300

    nothing

--- include/parsing.h
+++ include/parsing.h
@@ -1,6 +1,11 @@
 // Parsing utilities include:
 // - FEN string parsing into BoardState with bitboards
 // - Conversion from BoardState with bitboards back to FEN string
+
+/*
+FEN: rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1 <------------------------ full move number
+                                                            /----------------------------- half move clock
+*/
 #pragma once
 #include "tools.h"
 #include <string>
